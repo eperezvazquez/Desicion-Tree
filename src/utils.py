0317a1eb6f903dc95c33c1c54113c085950fb24a
eperezvazquez <perezelisa21@gmail.com>
2022-08-03 20:30:49 +0000

    forcasting

--- src/utils.py
+++ src/utils.py
@@ -11,5 +11,14 @@ class Helpers():
             return mean_diab
         else:
             return data_value
+            
+    @classmethod
+    def insulina(self, insulin_value, outcome_value, insuline_mean_0,insuline_mean_no0):
+        if outcome_value==0 and insulin_value==0:
+            return insuline_mean_0
+        elif outcome_value==1 and insulin_value==0:
+            return insuline_mean_no0
+        else:
+            return insulin_value
   
 
